9c8eb0f1ee309c31b35a961cd8a797d17e0b0d53
Jerry_Wang <Administrator@PC-201604301005>
2017-03-23 17:18:35 +0800

    appid secret

--- src/com/webChatUtils/WebChatPay.java
+++ src/com/webChatUtils/WebChatPay.java
@@ -1,8 +1,8 @@
 package com.webChatUtils;
 
 public class WebChatPay {
-	public static final String appid = "XXXXXX";// 在微信开发平台登记的app应用
-	public static final String appsecret = "XXXXXXXXX";
+	public static final String appid = "wx1c3baaad72ede441";// 在微信开发平台登记的app应用
+	public static final String appsecret = "13b7052c4376004a36024e419045f87e";
 	public static final String partner = "XXXXXXXXX";// 商户号
 	public static final String partnerkey = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX";// 不是商户登录密码，是商户在微信平台设置的32位长度的api秘钥，
 	public static final String createOrderURL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
